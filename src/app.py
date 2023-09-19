6e435d6cf6d0a46fb4dad0973e953657b1160ab7
Pedro Lealdino Filho <admin@dataverso.com.br>
2023-09-18 23:09:54 -0300

    new connection files

--- src/app.py
+++ src/app.py
@@ -1,2 +1,17 @@
-import requests
+from limesurveyrc2api.limesurvey import LimeSurvey
 
+url = "https://pb.utfpr.edu.br/geppadem/alimentario/index.php/admin/remotecontrol"
+username = "ppgdr-pb@utfpr.edu.br"
+password = "5MnVpxad273a"
+
+# Open a session.
+api = LimeSurvey(url=url, username=username)
+api.open(password=password)
+
+# Get a list of surveys the admin can see, and print their IDs.
+result = api.survey.list_surveys()
+for survey in result:
+    print(survey.get("sid"))
+
+# Close the session.
+api.close()
