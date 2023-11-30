5cc45c39bf350acbe27580f032ae73ad09a05022
Pedro Lealdino Filho <admin@dataverso.com.br>
2023-11-29 21:52:28 -0300

    update files

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
-        install_requires=['pandas', 'requests', 'json'], # add any additional packages that 
+        install_requires=['pandas', 'requests'], # add any additional packages that 
         # needs to be installed along with your package. Eg: 'caer'
         
         keywords=['python', 'first package'],
